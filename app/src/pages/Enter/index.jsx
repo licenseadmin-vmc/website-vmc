ad43803485d41476bca812ff699006f932dcdf2c
Greg Anderson <ganderson@Greg-Anderson-M1.local>
2022-06-04 13:08:21 -0700

    grid

--- app/src/pages/Enter/index.jsx
+++ app/src/pages/Enter/index.jsx
@@ -22,14 +22,63 @@ function Enter() {
         <div className="load"></div>
       ) : (
         <>
-          <Footer />
 
           <div className="flex-container">
+
+            <div className="quadrant-1 delay-q1">
+              <div>
+                <h1>IVD Compliance</h1>
+                <p>IVDR enacted May 26, 2022<br />
+                  Requirement for post-market surveyance of all IVD</p>
+                <a>Tell me about it</a>
+              </div>
+            </div>
+
+            <div className="quadrant-2 delay-q2">
+              <div>
+                <h1>VitroMetrics Opportunity</h1>
+                <p>IVD is a USD 117B market, which will grow to 128B by 2028<br />
+                  Compliance cost estimated at USD 3.5B – 3% of revenues<br />
+                  Compliance market estimated at USD 900M<br />
+                  EQA (External Quality Assessment) data is fit for purpose</p>
+                <a>More Info</a>
+              </div>
+            </div>
+
+            <div className="quadrant-3 delay-q3">
+              <div>
+                <h1>VitroMetrics Advantage</h1>
+                <p>30 years of regulatory experience<br />
+                  20 years of EQA data
+                </p>
+                <a>Explore</a>
+              </div>
+            </div>
+
+            <div className="quadrant-4 delay-q4">
+              <div>
+                <h1>VitroMetrics Datacube</h1>
+                <p>AI analytics applied to big data<br />
+                  Mutliple facets of data collected<br />
+                  Multiple compliance regions satified with single solution</p>
+                <a id="selected">Get Started</a>
+              </div>
+            </div>
+
+
             <div className="row">
 
               <div className="flex-item">
 
-                <img className='fade-in' src='img/logo-vm.png' height='72' /> <div className='nudge-sm'></div>
+
+                <img class='fade-in' src='img/logo-vm.png' height='72' /> <div class='nudge-md'></div>
+
+
+
+              </div>
+
+              <div className="follow">
+
 
                 <Typewriter
                   options={{
@@ -44,28 +93,35 @@ function Enter() {
                       .typeString("<h1>Your IVDR Solution</h1>")
                       .pauseFor(400)
                       .typeString("<h4>powered by <strong>VitroMetrics</strong></h4>")
-                      .pauseFor(2500)
+                      .pauseFor(1000)
                       .deleteAll(1)
-                      .pasteString("<h2 class='fade-in'>As of May 26th 2022, the new In Vitro Diagnostics Regulation is in effect.</h2>")
-                      .pauseFor(750)
-                      .pasteString("<div class='nudge-sm'></div>")
-                      .pasteString("<h2 class='fade-in'><b>VitroMetrics has your complete solution.</b></h2>")
+                      .pasteString("<h2 class='fade-in'>As of May 26th 2022, <strong> In Vitro Diagnostics Regulation </strong> is in effect.</h2>")
                       .pauseFor(750)
                       .pasteString("<div class='nudge-md'></div>")
-                      .typeString("<h3>More Coming Soon <span class='pulse'>_</span</h3>")
-
+                      .pasteString("<h2 class='pulse-turbo'><strong>VitroMetrics has your complete solution.</strong></h2>")
+                      .pauseFor(2200)
+                      .deleteAll(1)
                       .start();
                   }}
                 />
 
               </div>
 
+
             </div>
 
-          </div>
+            <div className="flex-container-lineX"></div>
+            <div className="flex-container-lineY"></div>
+
+
+          </div >
+
+          <Footer />
+
 
         </>
-      )}
+      )
+      }
     </>
   );
 }
