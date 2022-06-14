8c014275205efd76cc6fc9dd432b2158514016a0
Victor Joseph <vjoseph@1wa.org>
2022-06-14 20:02:03 +0100

    meta tag & tag manager

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TagManager from 'react-gtm-module'
 
 import App from './components/App'
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-)
+TagManager.initialize({
+  gtmId: 'GTM-MLFLVWP'
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- app/src/pages/Enter/index.jsx
+++ app/src/pages/Enter/index.jsx
@@ -1,211 +1,285 @@
-import React, { useState, useEffect, Fragment } from "react";
-import Footer from "../Footer";
+import React, { useState, useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 import axios from 'axios'
-import Typewriter from "typewriter-effect";
+import Typewriter from 'typewriter-effect'
 
-function Enter() {
+import Footer from '../Footer'
 
-  const [loading, setLoading] = useState(true);
-  const [selectedQuadrant, setSelectedQuadrant] = useState(0);
-  const [quadrants, setQuadrants] = useState(false);
-  const [quadrantLines, setQuadrantLines] = useState(false);
-  const [content, setContent] = useState([]);
+function Enter () {
+  const [loading, setLoading] = useState(true)
+  const [selectedQuadrant, setSelectedQuadrant] = useState(0)
+  const [quadrants, setQuadrants] = useState(false)
+  const [quadrantLines, setQuadrantLines] = useState(false)
+  const [content, setContent] = useState([])
 
   useEffect(() => {
-
-    setLoading(false);
+    setLoading(false)
 
     setTimeout(() => {
       setQuadrants(true)
-    }, 5700);
+    }, 5700)
 
     setTimeout(() => {
       setQuadrantLines(true)
-    }, 5300);
+    }, 5300)
 
-
-    (async function () {
+    ;(async function () {
       try {
-        const response = await axios.get(`data/content.json?` + (Math.floor(Math.random() * 100000)));
-        setContent(response.data);
+        const response = await axios.get(
+          `data/content.json?` + Math.floor(Math.random() * 100000)
+        )
+        setContent(response.data)
       } catch (err) {
-        console.error(err);
+        console.error(err)
       }
-    })();
+    })()
+  }, [])
 
-  }, []);
-
-  function handleQuadrant(event) {
+  function handleQuadrant (event) {
     setSelectedQuadrant(event)
   }
 
-  return content.length > 0 && (
-    <>
-      {loading ?
-        <div className="flex-container">
-          <div className="load"></div>
-        </div>
-        :
-        <>
+  return (
+    content.length > 0 && (
+      <>
+        <Helmet titleTemplate='VitroMetrics | %s'>
+          <title>Home</title>
+        </Helmet>
 
-          <div className="flex-container">
+        {loading ? (
+          <div className='flex-container'>
+            <div className='load'></div>
+          </div>
+        ) : (
+          <>
+            <div className='flex-container'>
+              {quadrants ? (
+                <>
+                  {selectedQuadrant == 1 ? (
+                    <>
+                      <div className='quadrant-select-1'></div>
 
-            {quadrants ?
-              <>
+                      <div className='quadrant-1'>
+                        <div>
+                          <h1>{content[1].title}</h1>
+                          <p
+                            className='fade-in'
+                            dangerouslySetInnerHTML={{
+                              __html: content[1].body
+                            }}
+                          />
 
-                {selectedQuadrant == 1 ?
-                  <>
-                    <div className="quadrant-select-1"></div>
-
-                    <div className="quadrant-1">
-                      <div>
-                        <h1>{content[1].title}</h1>
-                        <p className="fade-in" dangerouslySetInnerHTML={{ __html: content[1].body }} />
-
-                        <a onClick={() => handleQuadrant(0)}>{content[1].buttonBack}</a>
+                          <a onClick={() => handleQuadrant(0)}>
+                            {content[1].buttonBack}
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                  : selectedQuadrant == 0 &&
-                  <div className="quadrant-1 fade-in">
-                    <div>
-                      <h1>{content[1].title}</h1>
-                      <p dangerouslySetInnerHTML={{ __html: content[1].abstract }} />
-                      <a onClick={() => handleQuadrant(1)}>{content[1].buttonCTA}</a>
-                    </div>
-                  </div>
-                }
-                {selectedQuadrant == 2 ?
-                  <>
-                    <div className="quadrant-select-2"></div>
-
-                    <div className="quadrant-2">
-                      <div>
-                        <h1>{content[2].title}</h1>
-
-                        <p className="fade-in" dangerouslySetInnerHTML={{ __html: content[2].body }} />
-                        <a onClick={() => handleQuadrant(0)}>{content[2].buttonBack}</a>
-
+                    </>
+                  ) : (
+                    selectedQuadrant == 0 && (
+                      <div className='quadrant-1 fade-in'>
+                        <div>
+                          <h1>{content[1].title}</h1>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html: content[1].abstract
+                            }}
+                          />
+                          <a onClick={() => handleQuadrant(1)}>
+                            {content[1].buttonCTA}
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                  : selectedQuadrant == 0 &&
-                  <div className="quadrant-2 fade-in">
-                    <div>
-                      <h1>{content[2].title}</h1>
-                      <p dangerouslySetInnerHTML={{ __html: content[2].abstract }} />
-                      <a onClick={() => handleQuadrant(2)}>{content[2].buttonCTA}</a>
-                    </div>
-                  </div>
-                }
-                {selectedQuadrant == 3 ?
-                  <>
-                    <div className="quadrant-select-3"></div>
+                    )
+                  )}
+                  {selectedQuadrant == 2 ? (
+                    <>
+                      <div className='quadrant-select-2'></div>
 
-                    <div className="quadrant-3b">
-                      <div>
-                        <h1>{content[3].title}</h1>
-                        <p className="fade-in" dangerouslySetInnerHTML={{ __html: content[3].body }} />
+                      <div className='quadrant-2'>
+                        <div>
+                          <h1>{content[2].title}</h1>
 
-                        <a onClick={() => handleQuadrant(0)}>{content[3].buttonBack}</a>
+                          <p
+                            className='fade-in'
+                            dangerouslySetInnerHTML={{
+                              __html: content[2].body
+                            }}
+                          />
+                          <a onClick={() => handleQuadrant(0)}>
+                            {content[2].buttonBack}
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                  : selectedQuadrant == 0 &&
-                  <div className="quadrant-3 fade-in">
-                    <div>
-                      <h1>{content[3].title}</h1>
-                      <p dangerouslySetInnerHTML={{ __html: content[3].abstract }} />
-                      <a onClick={() => handleQuadrant(3)}>{content[3].buttonCTA}</a>
-                    </div>
-                  </div>
-                }
-                {selectedQuadrant == 4 ?
-                  <>
-                    <div className="quadrant-select-4"></div>
-
-                    <div className="quadrant-4b">
-                      <div>
-                        <h1>{content[4].title}</h1>
-
-                        <p className="fade-in" dangerouslySetInnerHTML={{ __html: content[4].body }} />
-
-                        <a onClick={() => handleQuadrant(0)}>{content[4].buttonBack}</a>
+                    </>
+                  ) : (
+                    selectedQuadrant == 0 && (
+                      <div className='quadrant-2 fade-in'>
+                        <div>
+                          <h1>{content[2].title}</h1>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html: content[2].abstract
+                            }}
+                          />
+                          <a onClick={() => handleQuadrant(2)}>
+                            {content[2].buttonCTA}
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                  : selectedQuadrant == 0 &&
-                  <div className="quadrant-4 fade-in">
-                    <div>
-                      <h1>{content[4].title}</h1>
-                      <p dangerouslySetInnerHTML={{ __html: content[4].abstract }} />
-                      <a onClick={() => handleQuadrant(4)}>{content[4].buttonCTA}</a>
-                    </div>
-                  </div>
-                }
-              </>
-              :
-              <div className="follow">
-                {selectedQuadrant == 0 &&
-                  <>
-                    <Typewriter
-                      options={{
-                        autoStart: true,
-                        loop: false,
-                        cursor: "",
-                        delay: 12,
-                      }}
-                      onInit={(typewriter) => {
-                        typewriter
-                          .pauseFor(200)
-                          .typeString("<h1>" + content[0].title + "</h1>")
-                          .pauseFor(400)
-                          .typeString("<h4>" + content[0].subtitle + "</h4>")
-                          .pauseFor(1000)
-                          .deleteAll(1)
-                          .pasteString("<h2 class='fade-in'>" + content[0].line1 + "</h2>")
-                          .pauseFor(750)
-                          .pasteString("<div class='nudge-md'></div>")
-                          .pasteString("<h2 class='pulse-turbo'>" + content[0].line2 + "</h2>")
+                    )
+                  )}
+                  {selectedQuadrant == 3 ? (
+                    <>
+                      <div className='quadrant-select-3'></div>
 
-                          .pauseFor(1700)
-                          .deleteAll(1)
-                          .start();
-                      }}
-                    />
-                  </>
-                }
+                      <div className='quadrant-3b'>
+                        <div>
+                          <h1>{content[3].title}</h1>
+                          <p
+                            className='fade-in'
+                            dangerouslySetInnerHTML={{
+                              __html: content[3].body
+                            }}
+                          />
 
+                          <a onClick={() => handleQuadrant(0)}>
+                            {content[3].buttonBack}
+                          </a>
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    selectedQuadrant == 0 && (
+                      <div className='quadrant-3 fade-in'>
+                        <div>
+                          <h1>{content[3].title}</h1>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html: content[3].abstract
+                            }}
+                          />
+                          <a onClick={() => handleQuadrant(3)}>
+                            {content[3].buttonCTA}
+                          </a>
+                        </div>
+                      </div>
+                    )
+                  )}
+                  {selectedQuadrant == 4 ? (
+                    <>
+                      <div className='quadrant-select-4'></div>
+
+                      <div className='quadrant-4b'>
+                        <div>
+                          <h1>{content[4].title}</h1>
+
+                          <p
+                            className='fade-in'
+                            dangerouslySetInnerHTML={{
+                              __html: content[4].body
+                            }}
+                          />
+
+                          <a onClick={() => handleQuadrant(0)}>
+                            {content[4].buttonBack}
+                          </a>
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    selectedQuadrant == 0 && (
+                      <div className='quadrant-4 fade-in'>
+                        <div>
+                          <h1>{content[4].title}</h1>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html: content[4].abstract
+                            }}
+                          />
+                          <a onClick={() => handleQuadrant(4)}>
+                            {content[4].buttonCTA}
+                          </a>
+                        </div>
+                      </div>
+                    )
+                  )}
+                </>
+              ) : (
+                <div className='follow'>
+                  {selectedQuadrant == 0 && (
+                    <>
+                      <Typewriter
+                        options={{
+                          autoStart: true,
+                          loop: false,
+                          cursor: '',
+                          delay: 12
+                        }}
+                        onInit={typewriter => {
+                          typewriter
+                            .pauseFor(200)
+                            .typeString('<h1>' + content[0].title + '</h1>')
+                            .pauseFor(400)
+                            .typeString('<h4>' + content[0].subtitle + '</h4>')
+                            .pauseFor(1000)
+                            .deleteAll(1)
+                            .pasteString(
+                              "<h2 class='fade-in'>" +
+                                content[0].line1 +
+                                '</h2>'
+                            )
+                            .pauseFor(750)
+                            .pasteString("<div class='nudge-md'></div>")
+                            .pasteString(
+                              "<h2 class='pulse-turbo'>" +
+                                content[0].line2 +
+                                '</h2>'
+                            )
+
+                            .pauseFor(1700)
+                            .deleteAll(1)
+                            .start()
+                        }}
+                      />
+                    </>
+                  )}
+                </div>
+              )}
+
+              <div className='row'>
+                <div className={`flex-item-q` + selectedQuadrant}>
+                  <img
+                    className='zoom-out'
+                    style={{
+                      cursor: selectedQuadrant !== 0 ? 'pointer' : 'default'
+                    }}
+                    onClick={() => handleQuadrant(0)}
+                    src='img/logo-vm.png'
+                    height='72'
+                  />{' '}
+                  <div className='nudge-md'></div>
+                </div>
               </div>
 
-            }
-
-            <div className="row">
-
-              <div className={`flex-item-q` + selectedQuadrant}>
-
-                <img className="zoom-out" style={{ cursor: selectedQuadrant !== 0 ? "pointer" : "default" }} onClick={() => handleQuadrant(0)} src='img/logo-vm.png' height='72' /> <div className='nudge-md'></div>
-
-              </div>
-
+              {quadrantLines && (
+                <>
+                  <div
+                    className={`flex-container-lineX-q` + selectedQuadrant}
+                  ></div>
+                  <div
+                    className={`flex-container-lineY-q` + selectedQuadrant}
+                  ></div>
+                </>
+              )}
             </div>
 
-            {quadrantLines &&
-              <>
-                <div className={`flex-container-lineX-q` + selectedQuadrant}></div>
-                <div className={`flex-container-lineY-q` + selectedQuadrant}></div>
-              </>
-            }
-
-          </div >
-
-          <Footer />
-
-        </>
-
-      }
-    </>
-  );
+            <Footer />
+          </>
+        )}
+      </>
+    )
+  )
 }
 
-export default Enter;
+export default Enter
